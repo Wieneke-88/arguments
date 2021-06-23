a92c07ca073dd4fe342a8faa42b0199e48960a9d
Wieneke-88 <84339361+Wieneke-88@users.noreply.github.com>
2021-06-23 23:16:39 +0200

    Add files via upload
    exercise using arguments in functions

---
+++ arguments.js
@@ -0,0 +1,32 @@
+console.log('hello');
+
+const paintWalls = function (sides, color) {
+    console.log("The " + sides + " wall has been painted " + color);
+};
+
+paintWalls("yellow");
+paintWalls("blue");
+paintWalls();
+paintWalls("north", "blue");
+
+const numberHight = function (number) {
+    const value = 93
+    if (number > 100) {
+        return; ("True")
+    } else if (number < 100) {
+        return; ("False")
+    };
+};
+
+const result = numberHight();
+console.log(result);
+
+const enteringPeople = function ('maximum', 'current', 'age') {
+    if (maximum = > )
+};
+
+const maximum = 60
+const current = 50
+const age = 18
+
+const result = enteringPeople();
